8fc0485ef458fa58d89448713140264bb5576cf5
Malcolm <forster.malcolm@gmail.com>
2022-08-16 07:10:40 +1200

    Copied across website
    Copied webpage from webserver

---
+++ index.php
@@ -0,0 +1,135 @@
+
+<!DOCTYPE html>
+<html>
+<link rel="stylesheet" href="formatting/MainFormat.css?v=<?php echo time(); ?>">
+<script type="text/javascript" src="jquery-3.6.0.min.js"></script>
+<script type="text/javascript" src="script.js"></script>
+<head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <title>Malcolm's Website</title> 
+        
+</head>
+
+<!-- 
+    This website is to be in parallax of all the projects I have worked on:
+
+    Projects Added to Website:
+
+
+    Projects Working on adding:
+
+    Projects to add:
+    
+        Fishr
+        FuelExchange
+        CommunityGardens
+        CloudForce
+        SimpleSync
+        Token Miner
+        Welded Joint Designer
+
+ -->
+
+
+
+<body>
+
+
+
+    
+<div class="header">
+    <h1>Malcolm's Website</h1>
+</div>
+
+<div class="topbacking transparent">
+    <p class="transparent">Testing</p>
+</div>
+
+<div class="gradient transparent">
+    <p class="transparent">Testing</p>
+</div>
+
+<div class="wrapper">  
+
+    <nav class="sidenav">
+        <a href="#top">Top</a><br>
+        <a href="#Fishr">Fishr</a><br>
+        <a href="#FuelExchange">FuelExchange</a><br>
+        <a href="#SimpleSync">SimpleSync</a><br>
+        <a href="#TokenMiner">TokenMiner</a><br>
+        <a href="#Welded Joint Designer">Welded Joint Designer</a><br>
+    </nav> 
+
+    <div class="main">
+
+        <div class = "topsection section">
+            <h3>A website to access the websites and software designed and created by Malcolm Forster</h3>
+
+            <p>Hello, welcome to this small website which is meant to show the off my skills in the world of Information Technology.<br><br>
+            Please check out the websites below. These were to help me understand and learn about how a webserver is run efficiently as well as the programming required for both frontend and backend development.<br><br>
+            There are multiple databases in use, each customised to suit each website's purpose. A vast array of programming languages and development stacks have been used in these webpages to look into functionality, form and security. An explaination of each website is given in in the list below, as well as the methods used to create them.
+            </p>
+            <p>Hello, welcome to this small website which is meant to show the off my skills in the world of Information Technology.<br><br>
+            Please check out the websites below. These were to help me understand and learn about how a webserver is run efficiently as well as the programming required for both frontend and backend development.<br><br>
+            There are multiple databases in use, each customised to suit each website's purpose. A vast array of programming languages and development stacks have been used in these webpages to look into functionality, form and security. An explaination of each website is given in in the list below, as well as the methods used to create them.
+            </p>
+        </div>
+
+        <div class="section">       
+            <div class = "section bgFish">
+                <p class = "transparent"></p>
+                <h2 id="Fishr" class = "other"><a href = "../Fishr" target="_blank">Fishr</a></h2>
+            </div>
+            <h3>Fishr - The only website for fishing</h3>
+            <p>Fishr is a website designed to help fishermen(and fisherwoman!) keep track of their best catches! Not only this, it automatically keeps track of the weather and other conditions that led to that PB, and will alert when similar conditions are around the corner!</br></br>
+            Dubbed the facebook of the fishing world, this intuitive website has a range of features for the most mad keen of us. Organise fishing trips with friend easily, this will pass on all information such as conditions and weather to everyone invited and allows for alterations to the game plan by everyone.</br></br>
+            Using the most accurate information available, this site is a must have tool for anyone who is looking for the best day fishing!
+            </p>
+        </div>
+
+ 
+        <div class="section">
+            <div class = "section bgFE"><p class = "transparent"></p>
+                <h2 id="FuelExchange" class = "other"><a href = "../FuelExchange" target="_blank">Fuel Exchange</a></h2>
+            </div>
+        <p>FuelExchange is a crypto currency exchange platform that is simple for everyone to be able to use. It's priority to be create a secure and efficent menthod to trade and exchange crypto currencies of all types.<br><br>
+        It is also the home to my own cryptocurrency token, Token! This is a highly secure and ecofriendly PoW cryptocurrency that is aiming to take the world by storm, all the of the benifits of cryptocurrency, with none of the drawbacks!<br><br>
+        Token also has a mining application that is also displayed on this page.
+        </div> 
+
+        <div class="section">
+            <div  class = "section bgSS ">                <p class = "transparent"></p>
+                <h2 class = "other" id="SimpleSync"><a href = "../Fishr" target="_blank">SimpleSync</a></h2>
+            </div>
+            <p>Hello, welcome to this small website which is meant to show the off my skills in the world of Information Technology.<br><br>
+            Please check out the websites below. These were to help me understand and learn about how a webserver is run efficiently as well as the programming required for both frontend and backend development.<br><br>
+            There are multiple databases in use, each customised to suit each website's purpose. A vast array of programming languages and development stacks have been used in these webpages to look into functionality, form and security. An explaination of each website is given in in the list below, as well as the methods used to create them.
+            </p>
+        </div>
+
+        <div class="section">
+            <div  class = "section bgTM">                <p class = "transparent"></p>
+                <h2 class = "other" id="TokenMiner"><a href = "../FuelExchange" target="_blank">TokenMiner</a></h2>
+            </div>
+        <p>FuelExchange is a crypto currency exchange platform that is simple for everyone to be able to use. It's priority to be create a secure and efficent menthod to trade and exchange crypto currencies of all types.<br><br>
+        It is also the home to my own cryptocurrency token, Token! This is a highly secure and ecofriendly PoW cryptocurrency that is aiming to take the world by storm, all the of the benifits of cryptocurrency, with none of the drawbacks!<br><br>
+        Token also has a mining application that is also displayed on this page.
+        </div>  
+
+        <div class="section">
+            <div  class = "section bgWJD ">                <p class = "transparent"></p>
+                <h2 class = "other" id="Welded Joint Designer"><a href = "../Fishr" target="_blank">Welded Joint Designer</a></h2>
+            </div>
+            <p>Hello, welcome to this small website which is meant to show the off my skills in the world of Information Technology.<br><br>
+            Please check out the websites below. These were to help me understand and learn about how a webserver is run efficiently as well as the programming required for both frontend and backend development.<br><br>
+            There are multiple databases in use, each customised to suit each website's purpose. A vast array of programming languages and development stacks have been used in these webpages to look into functionality, form and security. An explaination of each website is given in in the list below, as well as the methods used to create them.
+            </p>
+        </div>
+    </div>
+</div>
+
+</body>
+
+
+</html>
---
+++ script.js
@@ -0,0 +1,93 @@
+//Sidebar to always be in middle regardless of its length nd where page is scrolled to
+
+$(window).on('load', function() {
+
+    $('.topsection').each(function() {
+        var topsection = $(this);
+        //var trueStart = other.offset().top;
+       // var docLoc = $(document).scrollTop();
+        var topEleParent = $(this).parent();
+
+        $(window).on({
+            scroll:function() {
+                var topSScroll = $(this).scrollTop();
+
+                //console.log(topSScroll);
+
+                var topSOpa = Math.min(1-(topSScroll-300)/160,1);
+
+                var trans = Math.max(0,Math.min(1-(topSScroll-300)/400,1));
+
+                    //console.log(1-((startPos - start)*0.05))
+
+                topsection.css({
+                    opacity: topSOpa,
+                    transform: 'scale('+trans+')'
+                });
+
+                
+            }
+        });
+
+
+    });
+
+
+    $('.other').each(function() {
+        var other = $(this);
+        //var trueStart = other.offset().top;
+        // var docLoc = $(document).scrollTop();
+        var otherEleParent = $(this).parent();
+        var otherParOffset = otherEleParent.offset().top;
+        // var startPos = other.offset().top;
+        
+
+        
+        //maybe set a variable for each var? ONe that increase ad decreases in respect with the totop
+        //The difference in before and after is how much is minus off whatever.
+            
+
+        $(window).on({
+            scroll:function() {
+
+                var otherPos = other.offset().top;
+                var otherScroll = $(this).scrollTop();
+                //var otherchange = otherPos - otherScroll - otherParOffset;
+
+                otherchange = otherPos - otherScroll;
+
+
+
+                //console.log(otherScroll);
+
+                var otherOpa = Math.min((otherchange)/100,1);
+                //console.log(otherOpa);
+                other.css({
+                    opacity: otherOpa
+                });
+                
+            }
+        });
+
+    });
+
+    $(window).scroll(function(){
+        var scrollTop = $(document).scrollTop();
+
+        var anchors = $('body').find('h2');
+        var classActive = $('a.active').length;
+        
+        //console.log($(anchors[i-1]).attr('active'));
+
+        for(var i=0; i < anchors.length; i++) {
+            if (scrollTop > $(anchors[i]).offset().top - 1000 && scrollTop < $(anchors[i]).offset().top +$(anchors[i]).height()-20){
+                // console.log($(anchors[i-1]).hasClass('.active'));
+                if(classActive == 0) {
+                    $('nav a[href="#' + $(anchors[i]).attr('id')+'"]').addClass('active');           
+                }
+            } else {
+                $('nav a[href="#' + $(anchors[i]).attr('id')+'"]').removeClass('active');
+            }
+        }
+    });
+});
